64fb74ff6acbc5d1fea9c208d50b0e1c9ad99222
Mark Mitchell <withoutnations@gmail.com>
2018-09-19 13:17:45 +0100

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function configureKarma(config) {
     SauceSafariLatest: {
       base: 'SauceLabs',
       browserName: 'Safari',
-      platform: 'Mac 10.9',
+      platform: 'Mac 10.13',
     },
     SauceInternetExplorerLatest: {
       base: 'SauceLabs',
